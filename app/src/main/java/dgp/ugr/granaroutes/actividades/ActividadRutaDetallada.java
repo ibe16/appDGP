ac0fd93a2d1abc5067995f0c8644e5ebf5ab84a0
Guillermo Bueno Vargas <guillergood@hotmail.com>
2018-11-26 16:59:35 +0100

    Hecho interfaz de Login y parte de lógica.

--- app/src/main/java/dgp/ugr/granaroutes/actividades/ActividadRutaDetallada.java
+++ app/src/main/java/dgp/ugr/granaroutes/actividades/ActividadRutaDetallada.java
@@ -17,62 +17,12 @@ import dgp.ugr.granaroutes.R;
 
 public class ActividadRutaDetallada extends AppCompatActivity {
 
-    private static final String TAG = "DETALLADO";
-    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_actividad_ruta_detallada);
 
-        mAuth = FirebaseAuth.getInstance();
-
-        mAuth.createUserWithEmailAndPassword("guillergood@gmail.com", "guillergood")
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            //TODO updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(ActividadRutaDetallada.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            //TODO updateUI(null);
-                        }
-
-                        // ...
-                    }
-                });
-
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-            // Name, email address, and profile photo Url
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
-
-            // Check if user's email is verified
-            boolean emailVerified = user.isEmailVerified();
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getIdToken() instead.
-            String uid = user.getUid();
-        }
-
-    }
-
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        //TODO updateUI(currentUser);
     }
 
 }
